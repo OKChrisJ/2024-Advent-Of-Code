ab129c34b783a766c8f366d1da5d654a6ed73ac9
Chris Jeff <chrisjeff429@gmail.com>
2024-12-03 21:40:53 -0500

    s

--- prob_3/part1.py
+++ prob_3/part1.py
@@ -2,8 +2,17 @@ import sys
 import re
 
 elems = []
+occurs = []
+total = 0
 
 for line in sys.stdin:
     elems.append(line)
 
-print(elems)
+for word in elems:
+    occurs.append(re.findall("mul([0-9]{3},[0-9)]{3})", word))
+
+for times_func in occurs:
+    left_num, right_num = times_func[4:-1].split(',')
+    total += left_num * right_num
+
+print(total)
